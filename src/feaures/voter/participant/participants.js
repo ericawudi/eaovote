74d1a21b035e73b960e441cd9187648a22092f92
Awudi Eric Okyere <info@eoawudi.com>
2023-04-01 20:35:58 +0000

    Sticky header and zero participant implementation

--- src/feaures/voter/participant/participants.js
+++ src/feaures/voter/participant/participants.js
@@ -6,20 +6,20 @@ export default function Participants({ participants }) {
   const { alreadyVoted, handleVote, selectedParticipantId } =
     useParticipantsLogicHook(participants);
 
-  return (
-    participants && (
-      <Grid container rowSpacing={3}>
-        {participants.map((participant) => (
-          <Grid item key={participant.id} sm={12} md={6} lg={4} xl={3}>
-            <Participant
-              participant={participant}
-              alreadyVoted={alreadyVoted}
-              onSelect={handleVote}
-              isSelected={selectedParticipantId}
-            />
-          </Grid>
-        ))}
-      </Grid>
-    )
+  return participants.length > 0 ? (
+    <Grid container rowSpacing={3}>
+      {participants.map((participant) => (
+        <Grid item key={participant.id} sm={12} md={6} lg={4} xl={3}>
+          <Participant
+            participant={participant}
+            alreadyVoted={alreadyVoted}
+            onSelect={handleVote}
+            isSelected={selectedParticipantId}
+          />
+        </Grid>
+      ))}
+    </Grid>
+  ) : (
+    <div>No participant under this category</div>
   );
 }
