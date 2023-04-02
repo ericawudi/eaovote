0c2bd136e18c188ce39be9e28497b1e6fb335293
Awudi Eric Okyere <info@eoawudi.com>
2023-04-02 15:34:03 +0000

    removed json-server package and gave an alt prop to image

--- src/components/Error/Error.js
+++ src/components/Error/Error.js
@@ -5,7 +5,7 @@ function Error({ message }) {
   const randomInt = Math.floor(Math.random() * 14);
   return (
     <div className={classes.container}>
-      <img src={SVGs[randomInt]} className={classes.image} />
+      <img src={SVGs[randomInt]} className={classes.image} alt="Error emoji" />
       <div className={classes.errorMsg}>{message}</div>
       <div className={classes.enjoy}>EnJoy!!</div>
     </div>
